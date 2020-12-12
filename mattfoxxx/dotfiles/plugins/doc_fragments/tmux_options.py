f0b0869339dd1034614d3067e5135a2e1a925614
Matthias Mintert <matthias@mintert.net>
2020-12-12 16:00:11 +0100

    initial collection with tmux role

---
+++ mattfoxxx/dotfiles/plugins/doc_fragments/tmux_options.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Options for the tmux role
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+class ModuleDocFragment(object):
+
+    DOCUMENTATION = r'''
+options:
+  pager:
+    description:
+    - description for pager option
+'''
